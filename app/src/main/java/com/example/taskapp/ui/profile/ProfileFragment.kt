f2d3dad58c3d9a9dea954deeddc428a7566ee329
Mirlan12 <mirlan12991@gamil.com>
2023-03-07 16:38:22 +0600

    added hw1

---
+++ app/src/main/java/com/example/taskapp/ui/profile/ProfileFragment.kt
@@ -0,0 +1,50 @@
+package com.example.taskapp.ui.profile
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.taskapp.R
+import com.example.taskapp.databinding.FragmentProfileBinding
+import com.example.taskapp.utils.loadImage
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    private val launcher = registerForActivityResult<Intent, ActivityResult>(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK
+            && result.data != null
+        ) {
+            val photoUri = result.data?.data
+            binding.avatarAccount.loadImage(photoUri.toString())
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.cvImg.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            launcher.launch(intent)
+        }
+    }
+
+}
